83334c148ae56ed7af4713b1cccb8a09386257d7
WORLD0182 <161698386+world0182@users.noreply.github.com>
2024-10-26 15:45:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 import subprocess
 import os
 
-TOKEN = "BOTのTOKEN"
-SERVER_PATH = r"サーバーファイルのパス"
+TOKEN = ""
+SERVER_PATH = r"PATH"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,14 +20,14 @@ async def on_message(message):
     global server_process
     if message.content.startswith("/start"):
         server_process = start_server()
-        await message.channel.send("Minecraftサーバーを起動しました")
+        await message.channel.send("Minecraft server is up and running!")
     elif message.content.startswith("/stop"):
         if server_process is not None and server_process.poll() is None:
             stop_server(server_process)
-            await message.channel.send("Minecraftサーバーを停止しました")
+            await message.channel.send("Minecraft server has been shut down.")
             server_process = None
         else:
-            await message.channel.send("サーバーは既に停止しています")
+            await message.channel.send("Server is already down.")
 
 def start_server():
     return subprocess.Popen([SERVER_PATH])
